7d0c58ed235c9c9ae1a6247aa7d8bfb42bf92bd6
witexd <31552461+witexd@users.noreply.github.com>
2017-12-01 23:42:34 +0100

    init

---
+++ src/first.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Podaj imie: " );
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " +name);
+
+	}
+
+}
